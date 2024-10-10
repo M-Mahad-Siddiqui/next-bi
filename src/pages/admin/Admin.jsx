2ece8eb4806b282cc3f2c4c4f4297b4123595b1e
M-Mahad-Siddiqui <mahadsiddiqui21@gmail.com>
2024-10-10 19:02:09 +0500

    update admin css

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Corrected import
+import { useNavigate } from 'react-router-dom';
 import back from '../../assets/back.png';
 import AddProduct from '../../components/admin/AddProduct/AddProduct';
 import ViewOrder from '../../components/admin/ViewOrder/ViewOrder';
@@ -7,7 +7,6 @@ import ViewProducts from '../../components/admin/ViewProducts/ViewProducts';
 import ViewUsers from '../../components/admin/ViewUsers/ViewUsers';
 import './Admin.css';
 
-// Constants for selected views
 const VIEWS = {
   ADD_PRODUCT: 'AddProduct',
   VIEW_ORDER: 'ViewOrder',
@@ -17,23 +16,31 @@ const VIEWS = {
 
 function Admin() {
   const [select, setSelect] = useState('');
-  const navigate = useNavigate(); // Correctly using useNavigate
+  const navigate = useNavigate();
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
 
   return (
     <div className='adminBody'>
-      <div>
-        <div className='header'>
-          <img onClick={() => navigate('/')} className='back' src={back} alt="" />
-          <h1>Welcome to Admin Panel</h1>
+      <div className='header'>
+        <div className='hamburger' onClick={toggleSidebar}>
+          &#9776;
         </div>
-        <div className='main'>
-          <div className='sidebar'>
-            <span onClick={() => setSelect(VIEWS.ADD_PRODUCT)}>Add Product</span>
-            <span onClick={() => setSelect(VIEWS.VIEW_ORDER)}>View Orders</span>
-            <span onClick={() => setSelect(VIEWS.VIEW_USERS)}>View Users</span>
-            <span onClick={() => setSelect(VIEWS.VIEW_PRODUCTS)}>View Products</span>
-          </div>
-          <div className="body">
+        <img onClick={() => navigate('/')} className='back' src={back} alt="Back" />
+        <h1>Welcome to Admin Panel</h1>
+      </div>
+      <div className='main'>
+        <div className={`sidebar ${sidebarOpen ? 'show' : ''}`}>
+          <span onClick={() => setSelect(VIEWS.ADD_PRODUCT)}>Add Product</span>
+          <span onClick={() => setSelect(VIEWS.VIEW_ORDER)}>View Orders</span>
+          <span onClick={() => setSelect(VIEWS.VIEW_USERS)}>View Users</span>
+          <span onClick={() => setSelect(VIEWS.VIEW_PRODUCTS)}>View Products</span>
+        </div>
+        <div className='body'>
+          <div className='content'>
             {select === VIEWS.ADD_PRODUCT && <AddProduct />}
             {select === VIEWS.VIEW_ORDER && <ViewOrder />}
             {select === VIEWS.VIEW_PRODUCTS && <ViewProducts />}
